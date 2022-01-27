b1d0800602a160fd80375e591c1e51a04b624107
Austin Shih <austinhshih@gmail.com>
2022-01-27 01:39:52 -0800

    Began navbar

---
+++ my-website/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <Link to="/" className="navbar-logo">
+                    HOME
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    {click ? "big screen": "hamburger icon"}
+                </div>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar;
+
