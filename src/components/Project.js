17f62f01e504f515f33703bfa7ba90196f87acfd
Austin Shih <austinhshih@gmail.com>
2022-02-27 20:39:12 -0800

    Updated Resume

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,26 +4,31 @@ import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/io'
 
 function Project(props) {
     const [click, setClick] = useState(false);
-
     const handleClick = () => setClick(!click);
-    const collapse = () => setClick(false);
-    const buttonType = click? (<IoIosArrowUp className='button-symbol'/>) : (<IoIosArrowDown className='button-symbol'/>)
+    const linkText = props.hasGit? 'GITHUB-LINK' : '';
+
     return(
-        //<a href={props.href} target='_blank' className='project-link'>
         <div className={click ? 'project-container active' : 'project-container'} onClick={props.onClick}>
             <p className='project-name'>
                 {props.name}
             </p>
-            <img src={props.src}  alt={props.name} className='project-image'></img>
-            <div className='project-info-block'>
-                <p className='project-dates'>
-                    {props.date}
-                </p>
-                <p className='project-notes'>
-                    {props.notes}
-                </p>
-            </div>
-            <div className='project-description-block'>
+            <ul className='flexgrid'>
+                <li>
+                    <img src={props.src}  alt={props.name} className='project-image'></img>
+                </li>
+                <li>
+                    <div className='project-info-block'>
+                        <p className='project-dates'>
+                            {props.date}
+                        </p>
+                        <a href={props.href} target='_blank' className='project-link'> 
+                            {linkText}
+                        </a>
+                    </div>                
+                </li>
+            </ul>
+
+            <div className={click ? 'project-description-block active' : 'project-description-block inactive'}>
                 <p className='project-description' >
                     {props.children}
                 </p>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,46 +16,11 @@ function Projects() {
         <ul className='project-list'>
           <li className='project'>
             <Project
-              name='OVERWATCH TACTICAL CROUCH'
-              src={require('../images/overwatch-workshop.png')}
-              date='SUMMER 2019'
-              notes='COMPLETED'
-            >
-              After years of playing Overwatch, I set out to revitalize my friends’ 
-              waning interest in standard modes by using its workshop mode to code a 
-              ridiculous custom game-mode where players get eliminated, not by futuristic 
-              weapons of war, but by being sat on. I relished the uncontrollable laughter 
-              of my friends being paralyzed by sleep darts, helpless from the encroaching 
-              buttocks. Even something as simple as the “kill” condition took countless lines 
-              of code since I had to account for a frustrating amount of edge cases. 
-              Regardless, the experience exposed me to a new realm of digital creation 
-              and led to me learning CAD, Illustrator, and programming.
-            </Project>
-          </li>
-          <li className='project'>
-            <Project
-              name='C++ MINESWEEPER'
-              href='https://github.com/liandy1028/Hoffman-Minesweeper'
-              src={require('../images/minesweeper-image')}
-              date='DEC 2020'
-              notes='COMPLETED'
-            >
-              This project is a recreation of Minesweeper with an interactive UI made with C++ and the 
-              SFML library. Due to the effective modularized structure of our project, splitting the 
-              work up between 4 group members was seamless. This project was initially coded and tested 
-              with a simple text based UI in the terminal and switching over to a more interactive 
-              interface only required replacing the text UI functions to the interactive UI functions. 
-              C++ Minesweeper was the first fully functioning game I had created and it gave me a glimpse 
-              of what it’s like to code in a team setting.
-            </Project>
-          </li>
-          <li className='project'>
-            <Project
               name='RING DETECTION OPENCV'
               href='https://github.com/Future14473/Ultimate-Road-Runner/tree/dev/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ComputerVision'
               src={require('../images/ring-detection.png')}
               date='AUG 2020 - APR 2021'
-              notes='COMPLETED'
+              hasGit={true}
             >
               The Computer VIsion task for the 2020-2021 FTC robotics competition required an accurate and 
               reliable algorithm to consistently count orange rings in a stack. My main challenge was creating
@@ -73,7 +38,7 @@ function Projects() {
               href='https://github.com/Austin2Shih/Pancakebot-bot'
               src={require('../images/Pancakebot-image.PNG')}
               date='SUMMER 2020'
-              notes='COMPLETED'
+              hasGit={true}
             >
               Pancakebot-bot is a Python script that automatically farms Discord's Pancakebot by fishing
               and answering Trivia questions. I started by targeting Trivia which stumped me with impossible
@@ -87,11 +52,46 @@ function Projects() {
           </li>
           <li className='project'>
             <Project
+              name='C++ MINESWEEPER'
+              href='https://github.com/liandy1028/Hoffman-Minesweeper'
+              src={require('../images/minesweeper-image')}
+              date='DEC 2020'
+              hasGit={false}
+            >
+              This project is a recreation of Minesweeper with an interactive UI made with C++ and the 
+              SFML library. Due to the effective modularized structure of our project, splitting the 
+              work up between 4 group members was seamless. This project was initially coded and tested 
+              with a simple text based UI in the terminal and switching over to a more interactive 
+              interface only required replacing the text UI functions to the interactive UI functions. 
+              C++ Minesweeper was the first fully functioning game I had created and it gave me a glimpse 
+              of what it’s like to code in a team setting.
+            </Project>
+          </li>
+          <li className='project'>
+            <Project
+              name='OVERWATCH TACTICAL CROUCH'
+              src={require('../images/overwatch-workshop.png')}
+              date='SUMMER 2019'
+              hasGit={false}
+            >
+              After years of playing Overwatch, I set out to revitalize my friends’ 
+              waning interest in standard modes by using its workshop mode to code a 
+              ridiculous custom game-mode where players get eliminated, not by futuristic 
+              weapons of war, but by being sat on. I relished the uncontrollable laughter 
+              of my friends being paralyzed by sleep darts, helpless from the encroaching 
+              buttocks. Even something as simple as the “kill” condition took countless lines 
+              of code since I had to account for a frustrating amount of edge cases. 
+              Regardless, the experience exposed me to a new realm of digital creation 
+              and led to me learning CAD, Illustrator, and programming.
+            </Project>
+          </li>
+          <li className='project'>
+            <Project
               name='THE FALLING BLOCK GAME'
               href='https://github.com/Austin2Shih/The-Falling-Block-Game/tree/dev'
               src={require('../images/falling block game image.jpg')}
               date='DEC 2021 - PRESENT'
-              notes='IN PROGRESS'
+              hasGit={true}
             >
               The Falling Block Game is my first attempt at building a mobile video game in Unity. 
               The game takes place in a grid where blocks fall from above to form an infinite tower. 
